6d154a1a9b4154292abfc07fd16d5ac68bd18862
nhien261203 <dovannhien12345@gmail.com>
2025-05-18 14:57:38 +0700

    feat backend api product

---
+++ backend/demo/app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'description' => 'nullable',
+        ]);
+
+        $product = Product::create($data);
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,Product $product)
+    {
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(
+            [
+                'message' => 'Xoa thanh cong',
+            ]
+            );
+    }
+}
